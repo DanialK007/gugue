7c664783d3fa6483313a241e6986b4039a4969d9
DanialK007 <kh2496187@gmail.com>
2026-01-10 00:38:07 +0630

    v 1.1 - mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,18 @@ function page() {
         <div className="absolute -z-1 inset-0">
           <img src="bg.jpeg" alt="" className="size-full brightness-125" />
         </div>
-        <div className="absolute top-0 left-0 w-full z-0 flex justify-center lg:pt-24">
-          <div className="text-[12rem] font-black text-white text-r ose-500/80">
-            Wint Thandar
+        <div className="absolute top-0 left-0 w-full z-0 flex justify-center px-5 pt-24">
+          <div className="text-8xl md:text-[12rem] font-black text-white text-r ose-500/80">
+            Wint Thandar <span className="md:hidden">Htun</span>
           </div>
         </div>
-        <div className="absolute bottom-0 left-0 w-full z-2 flex justify-center items-end lg:pb-24">
-          <div className="text-[12rem] font-black text-white text-r ose-500/80">
+        <div className="absolute bottom-0 left-0 w-full z-2 hidden md:flex justify-center items-end px-5 pb-24">
+          <div className="text-8xl md:text-[12rem] font-black text-white text-r ose-500/80">
             Htun
           </div>
         </div>
         <div className="absolute pointer-events-none inset-0 z-1 bottom-0 flex items-end justify-center">
-          <img src="gugue.png" alt="" className="" />
+          <img src="gugue.png" alt="" className="h-150 md:h-200" />
         </div>
         {/* 
         <img
@@ -28,7 +28,7 @@ function page() {
       </div>
 
       {/* second page */}
-      <div className="h-220 relative">
+      <div className="h-220 overflow-hidden relative">
         <div className="absolute -z-1 inset-0">
           <img
             src="newspaper.png"
@@ -40,13 +40,13 @@ function page() {
         <img
           src="star.jpeg"
           alt=""
-          className="h-28 rotate-20 absolute z-10 left-20 bottom-20"
+          className="h-20 md:h-28 rotate-20 absolute z-10 left-10 md:left-20 bottom-180 md:bottom-20"
         />
 
         <img
           src="tape.jpeg"
           alt=""
-          className="h-58 rotate-20 absolute z-10 left-36 top-20"
+          className="h-32 md:h-58 rotate-20 absolute z-10 left-64 md:left-36 top-58 md:top-20"
         />
 
         <img
@@ -58,10 +58,10 @@ function page() {
         <img
           src="guCam.jpeg"
           alt=""
-          className="w-110 absolute z-10 left-1/2 -translate-x-1/2 bottom-0 sat urate-0 cutout-border"
+          className="w-80 md:w-110 absolute z-10 ms-5 md:ms-0 left-1/2 -translate-x-1/2 bottom-0 sat urate-0 cutout-border"
         />
 
-        <div className="absolute z-10 right-28 bottom-20 bg-white shadow rotate-10">
+        <div className="absolute z-10 right-65 md:right-28 -bottom-5 md:bottom-20 scale-70 md:scale-100 bg-white shadow rotate-10">
           <div className="w-40 h-56 relative text-sm p-4">
             <div className="">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
