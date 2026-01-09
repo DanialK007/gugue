c06bdbf8059e8afdf58b2f979e460a3a691a5842
DanialK007 <kh2496187@gmail.com>
2026-01-09 23:58:16 +0630

    v 1 - two page

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,84 @@
-import Image from "next/image";
-
-export default function Home() {
+function page() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div>
+      {/* first page */}
+      <div className="h-220 relative bg-cover bg-center overflow-hidden">
+        <div className="absolute -z-1 inset-0">
+          <img src="bg.jpeg" alt="" className="size-full brightness-125" />
+        </div>
+        <div className="absolute top-0 left-0 w-full z-0 flex justify-center lg:pt-24">
+          <div className="text-[12rem] font-black text-white text-r ose-500/80">
+            Wint Thandar
+          </div>
+        </div>
+        <div className="absolute bottom-0 left-0 w-full z-2 flex justify-center items-end lg:pb-24">
+          <div className="text-[12rem] font-black text-white text-r ose-500/80">
+            Htun
+          </div>
+        </div>
+        <div className="absolute pointer-events-none inset-0 z-1 bottom-0 flex items-end justify-center">
+          <img src="gugue.png" alt="" className="" />
+        </div>
+        {/* 
+        <img
+          src="star.jpeg"
+          alt=""
+          className="h-28 rotate-20 absolute z-10 left-20 bottom-20"
+        /> */}
+      </div>
+
+      {/* second page */}
+      <div className="h-220 relative">
+        <div className="absolute -z-1 inset-0">
+          <img
+            src="newspaper.png"
+            alt="news"
+            className="size-full object-cover opacity-60 brightness-70"
+          />
+        </div>
+
+        <img
+          src="star.jpeg"
+          alt=""
+          className="h-28 rotate-20 absolute z-10 left-20 bottom-20"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+
+        <img
+          src="tape.jpeg"
+          alt=""
+          className="h-58 rotate-20 absolute z-10 left-36 top-20"
+        />
+
+        <img
+          src="phone.jpeg"
+          alt=""
+          className="w-58 absolute z-10 right-20 top-0"
+        />
+
+        <img
+          src="guCam.jpeg"
+          alt=""
+          className="w-110 absolute z-10 left-1/2 -translate-x-1/2 bottom-0 sat urate-0 cutout-border"
+        />
+
+        <div className="absolute z-10 right-28 bottom-20 bg-white shadow rotate-10">
+          <div className="w-40 h-56 relative text-sm p-4">
+            <div className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+              fugit earum facilis in minima adipisci, esse expedita ratione
+              officiis voluptatibus provident.
+            </div>
+
+            <img
+              src="clip.png"
+              alt=""
+              className="h-20 absolute z-10 -right-8 -top-12"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
+
+export default page;
