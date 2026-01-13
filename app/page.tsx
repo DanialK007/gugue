09b27e6df6f02291b6f9150a73a0fe89fdc65183
DanialK007 <kh2496187@gmail.com>
2026-01-13 23:54:36 +0630

    v 1.3 - social links

--- app/page.tsx
+++ app/page.tsx
@@ -1,76 +1,93 @@
 import Hero from "@/components/Hero";
 import Scroller from "@/components/ui/Scroller";
-
 function page() {
   return (
     <>
       <Hero />
 
       {/* second page */}
-      <div className="h-220 overflow-hidden relative">
-        <div className="absolute -z-1 inset-0">
+      <div className="h-220 -mt-8 pt-6 relative">
+        <div className="absolute z-0 inset-0">
           <img
             src="newspaper.png"
             alt="news"
-            className="size-full object-cover opacity-60 brightness-70"
+            className="size-full object-cover opacity-30 brightness-70"
           />
         </div>
-
-        <div className="absolute z-10 left-64 md:left-36 top-58 md:top-20">
-          <Scroller fromY={400} startPx={-800} endPx={0}>
-            <img
-              src="tape.jpeg"
-              alt="tape"
-              className="h-32 md:h-58 rotate-20"
-            />
-          </Scroller>
-        </div>
-
-        <div className="absolute z-10 right-20 top-0">
-          <Scroller fromY={-350} startPx={400} endPx={800}>
-            <img src="phone.jpeg" alt="phone" className="w-58" />
-          </Scroller>
-        </div>
-
-        <div className="rotate-20 absolute z-10 left-10 md:left-20 bottom-120 md:bottom-20">
-          <Scroller fromX={-300} fromRotate={360} startPx={-300} endPx={-300}>
-            <img src="star.jpeg" alt="star" className="h-20 md:h-28" />
-          </Scroller>
-        </div>
-
-        <div className="absolute z-10 me-8 md:me-0 left-0 bottom-0 w-full flex justify-center px-10">
-          <Scroller fromY={300} startPx={-300} endPx={-300}>
-            <img
-              src="guCam.jpeg"
-              alt="gu"
-              className="w-full md:w-110 cutout-border"
-            />
-          </Scroller>
-        </div>
-
-        <div className="absolute z-10 right-0 md:right-28 -bottom-5 md:bottom-20 scale-70 md:scale-100 rotate-10">
-          <Scroller fromX={300} startPx={-300} endPx={-300}>
-            <div className="w-40 h-56 relative text-sm p-4 bg-white shadow-lg shadow-black/20">
-              <div className="font-schoolbell">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-                fugit earum facilis in minima adipisci, esse expedita ratione
-                officiis voluptatibus provident.
-              </div>
-
+        <div className="h-full overflow-hidden relative">
+          <div className="absolute z-10 left-64 md:left-36 top-58 md:top-20">
+            <Scroller fromY={400} startPx={-800} endPx={0}>
               <img
-                src="clip.png"
-                alt=""
-                className="h-20 absolute z-10 -right-8 -top-12"
+                src="tape.jpeg"
+                alt="tape"
+                className="h-32 md:h-58 rotate-20"
               />
-            </div>
-          </Scroller>
+            </Scroller>
+          </div>
+
+          <div className="absolute z-10 right-20 top-0">
+            <Scroller fromY={-350} startPx={400} endPx={800}>
+              <img src="phone.jpeg" alt="phone" className="w-58" />
+            </Scroller>
+          </div>
+
+          <div className="rotate-20 absolute z-10 left-10 md:left-20 bottom-120 md:bottom-20">
+            <Scroller fromX={-300} fromRotate={360} startPx={-300} endPx={-300}>
+              <img src="star.jpeg" alt="star" className="h-20 md:h-28" />
+            </Scroller>
+          </div>
+
+          <div className="absolute z-10 me-8 md:me-0 left-0 bottom-0 w-full flex justify-center px-10">
+            <Scroller fromY={300} startPx={-300} endPx={-300}>
+              <img
+                src="guCam.jpeg"
+                alt="gu"
+                className="w-full md:w-110 cutout-border"
+              />
+            </Scroller>
+          </div>
+
+          <div className="absolute z-9 inset-0 -translate-y-56 md:translate-y-0 text-4xl lg:text-8xl text-primary font-nerko flex gap-12 md:gap-120 items-center justify-center">
+            <Scroller fromX={360} startPx={-400} endPx={0}>
+              <div className="w-36 lg:w-86">Architect</div>
+            </Scroller>
+            <Scroller fromX={-360} startPx={-400} endPx={0}>
+              <div className="ps-10 w-36 lg:w-86">Artist</div>
+            </Scroller>
+          </div>
+
+          <div className="absolute z-10 right-0 md:right-28 -bottom-5 md:bottom-20 scale-70 md:scale-100 rotate-10">
+            <Scroller fromX={300} startPx={-300} endPx={-300}>
+              <div className="w-40 h-56 relative text-sm p-4 bg-white shadow-lg shadow-black/20">
+                <div className="font-schoolbell">
+                  Aspiring architect with a soul for art and an eye for detail.
+                  From the rhythm of music to the precision of a sketch, my work
+                  explores how different art forms can inspire better living
+                  spaces.
+                </div>
+
+                <img
+                  src="clip.png"
+                  alt=""
+                  className="h-20 absolute z-10 -right-8 -top-12"
+                />
+              </div>
+            </Scroller>
+          </div>
         </div>
       </div>
 
       <div className="p-5 bg-primary bg-[#b32432] text-white flex justify-between text-xs">
         <div className="">2026 @ Copyright</div>
         <div className="">
-          Made by <a href="kaungkhantkyaw.online" className="hover:underline font-bold font-schoolbell">Danial</a> ♥
+          Made by{" "}
+          <a
+            href="kaungkhantkyaw.online"
+            className="hover:underline font-bold font-schoolbell"
+          >
+            Danial
+          </a>{" "}
+          ♥
         </div>
       </div>
     </>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,41 +1,146 @@
+"use client";
 import Scroller from "./ui/Scroller";
+import { useRef } from "react";
+import Lottie, { LottieRefCurrentProps } from "lottie-react";
+import facebookAnim from "@/public/Facebook/facebook.json";
+import instagramAnim from "@/public/Instagram/instagram.json";
+import twitterAnim from "@/public/Twitter/twitter.json";
 
 function Hero() {
+  const facebookRef = useRef<LottieRefCurrentProps | null>(null);
+  const instagramRef = useRef<LottieRefCurrentProps | null>(null);
+  const twitterRef = useRef<LottieRefCurrentProps | null>(null);
+
   return (
-    <div className="h-220 relative bg-cover bg-center overflow-hidden">
-      <div className="absolute -z-1 inset-0">
-        <img src="bg.jpeg" alt="" className="size-full brightness-125" />
-      </div>
-      <div className="absolute font-nerko top-0 left-0 w-full z-0 flex justify-center px-20 pt-28">
-        <Scroller
-          // markers={true}
-          toX={-270}
-          startPx={300}
-          endPx={800}
-          startPercent={50}
-          endPercent={0}
-        >
-          <div className="text-8xl md:text-[12rem] font-black text-white">
-            Wint Thandar <span className="lg:hidden">Htun</span>
-          </div>
-        </Scroller>
-      </div>
-      <div className="absolute font-nerko bottom-0 left-0 w-full z-2 hidden lg:flex justify-center items-end px-5 pb-28">
-        <Scroller
-          // markers={true}
-          toX={300}
-          startPx={-200}
-          endPx={800}
-          startPercent={50}
-          endPercent={0}
-        >
-          <div className="text-8xl md:text-[12rem] font-black text-white px-20">
-            Htun
-          </div>
-        </Scroller>
-      </div>
-      <div className="absolute pointer-events-none inset-0 z-1 bottom-0 flex items-end justify-center">
-        <img src="gugue.png" alt="" className="h-150 md:h-200" />
+    <div className="p-2 bg-linear-to-b from-neutral-200 from-95% h-screen max-h-fit">
+      <div className="h-220 max-h-full rounded-3xl relative bg-cover bg-center overflow-hidden">
+        <div className="absolute z-1 inset-0 rounded-3xl">
+          <img src="bg.jpeg" alt="" className="size-full brightness-125" />
+        </div>
+        <div className="absolute font-nerko top-0 left-0 w-full z-2 flex justify-center px-20 pt-28">
+          <Scroller
+            // markers={true}
+            toX={-270}
+            startPx={300}
+            endPx={800}
+            startPercent={50}
+            endPercent={0}
+          >
+            <div className="text-8xl md:text-[12rem] font-black text-white">
+              Wint Thandar <span className="lg:hidden">Htun</span>
+            </div>
+          </Scroller>
+        </div>
+        <div className="absolute font-nerko bottom-0 left-0 w-full z-4 hidden lg:flex justify-center items-end px-5 pb-28">
+          <Scroller
+            // markers={true}
+            toX={300}
+            startPx={-200}
+            endPx={800}
+            startPercent={50}
+            endPercent={0}
+          >
+            <div className="text-8xl md:text-[12rem] font-black text-white px-20">
+              Htun
+            </div>
+          </Scroller>
+        </div>
+        <div className="absolute pointer-events-none inset-0 z-3 bottom-0 flex items-end justify-center">
+          <img src="gugue.png" alt="" className="h-120 md:h-186" />
+        </div>
+
+        <div className="absolute z-11 bottom-0 right-0 p-3 md:p-5 flex gap-3">
+          <Scroller
+            // markers={true}
+            toX={200}
+            toRotate={480}
+            startPx={-50}
+            endPx={800}
+            startPercent={70}
+            endPercent={0}
+          >
+            <a
+              href="#"
+              className="flex hover:scale-110 duration-300 bg-white border-2 border-black rounded-full"
+              onMouseEnter={() => {
+                facebookRef.current?.setDirection(1);
+                facebookRef.current?.play();
+              }}
+              onMouseLeave={() => {
+                facebookRef.current?.setDirection(-1);
+                facebookRef.current?.play();
+              }}
+            >
+              <Lottie
+                lottieRef={facebookRef}
+                animationData={facebookAnim}
+                loop={false}
+                autoplay={false}
+                className="size-8 md:size-9 pe-0.5 rounded-full"
+              />
+            </a>
+          </Scroller>
+          <Scroller
+            // markers={true}
+            toX={200}
+            toRotate={480}
+            startPx={-70}
+            endPx={800}
+            startPercent={70}
+            endPercent={0}
+          >
+            <a
+              href="#"
+              className="flex hover:scale-110 duration-300 bg-white border-2 border-black rounded-full"
+              onMouseEnter={() => {
+                instagramRef.current?.setDirection(1);
+                instagramRef.current?.play();
+              }}
+              onMouseLeave={() => {
+                instagramRef.current?.setDirection(-1);
+                instagramRef.current?.play();
+              }}
+            >
+              <Lottie
+                lottieRef={instagramRef}
+                animationData={instagramAnim}
+                loop={false}
+                autoplay={false}
+                className="size-8 md:size-9 p-0.5 rounded-full"
+              />
+            </a>
+          </Scroller>
+          <Scroller
+            // markers={true}
+            toX={200}
+            toRotate={480}
+            startPx={-90}
+            endPx={800}
+            startPercent={70}
+            endPercent={0}
+          >
+            <a
+              href="#"
+              className="flex hover:scale-110 duration-300 bg-white border-2 border-black rounded-full"
+              onMouseEnter={() => {
+                twitterRef.current?.setDirection(1);
+                twitterRef.current?.play();
+              }}
+              onMouseLeave={() => {
+                twitterRef.current?.setDirection(-1);
+                twitterRef.current?.play();
+              }}
+            >
+              <Lottie
+                lottieRef={twitterRef}
+                animationData={twitterAnim}
+                loop={false}
+                autoplay={false}
+                className="size-8 md:size-9 p-0.5 translate-x-0.5 rounded-full"
+              />
+            </a>
+          </Scroller>
+        </div>
       </div>
     </div>
   );
