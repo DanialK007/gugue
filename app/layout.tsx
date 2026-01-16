3db658d44f9d4fe844228e6e50bca98d4621e2a8
DanialK007 <kh2496187@gmail.com>
2026-01-17 00:30:08 +0630

    v 1.6.3 - vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import LenisWrapper from "@/components/ui/LenisWrapper";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         <div className="bg-white dark:bg-neutral-900">
           <LenisWrapper>{children}</LenisWrapper>
         </div>
